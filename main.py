093ad429ffcc90d2f1ca71f8dd4ac5aa58c232d3
Bruno Saint Clair <brunosaint@dcomp.ufs.br>
2024-09-14 14:14:49 -0300

    :recycle: refactor: alterei o exemplo utilizado na main

--- main.py
+++ main.py
@@ -1,39 +1,18 @@
 from Scraper import Scraper
 
-url = 'https://www.nfl.com/news/'
-
 scraper = Scraper()
 
-texto = scraper.get_content(url,'div', 'd3-o-media-object__body')
-lista_noticias = scraper.extract_news(texto, 'h3', 'p')
+url = 'https://www.nfl.com/news/'
 
-print()
-print('Notícias da NFL: ')
-print()
+texto = scraper.get_content(url, 'div', 'd3-o-media-object__body')
+lista_noticias = scraper.extract_news(texto, 'h3')
+
 scraper.show_news(lista_noticias)
 
 
+# Outro exemplo de uso:
 
-url = 'https://www.nba.com/'
-
-texto = scraper.get_content(url, 'h3', 'Text_text__I2GnQ ContentSlide_text__m_W0K')
-
-lista_noticias = scraper.extract_news(texto, 'span', 'MultiLineEllipsis_ellipsis___1H7z')
-
-print()
-print('Notícias da NBA: ')
-print()
-scraper.show_news(lista_noticias)
-
-
-
-url = 'https://ge.globo.com/'
-
-texto = scraper.get_content(url, 'div', 'feed-post-body')
-
-lista_noticias = scraper.extract_news(texto, 'a')
-
-print()
-print('Notícias do GE: ')
-print()
-scraper.show_news(lista_noticias)
+# url = 'https://ge.globo.com/'
+# texto = scraper.get_content(url, 'div', 'feed-post-body')
+# lista_noticias = scraper.extract_news(texto, 'a')
+# scraper.show_news(lista_noticias)
